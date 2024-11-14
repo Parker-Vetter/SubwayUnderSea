2c252ecd34057aed269c4c264b730a434b6b2e80
Aria-Williams <159070680+Area9579@users.noreply.github.com>
2024-11-14 11:39:39 -0600

    Squashed commit of the following:
    commit 7880ed0b199be103dcac6e124b34148d86a96247
    Author: Aria-Williams <159070680+Area9579@users.noreply.github.com>
    Date:   Thu Nov 14 11:33:39 2024 -0600
    
        Small Tweak to Sonar System
    
    commit b57024cbf025f31c799e1ac9c6f71403525d1a69
    Author: Aria-Williams <159070680+Area9579@users.noreply.github.com>
    Date:   Thu Nov 14 11:26:27 2024 -0600
    
        Sonar System

---
+++ subwaysea/Scripts/Supply Grabber/debris.gd
@@ -0,0 +1,49 @@
+extends Node2D
+
+const Director = preload("res://Scripts/director.gd")
+
+var mouse_hover = false
+
+signal debris_collected()
+
+@onready var is_monster = randi_range(0, 1)
+
+func _ready() -> void:
+	if is_monster:
+		position.x = randfn(0, 64)
+		position.y = 100
+	else:
+		position.x = randfn(0, 64)
+		position.y = -100
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("left_mouse") and mouse_hover:
+		debris_collected.emit()
+		self.queue_free()
+	
+
+func check_position():
+	if position.x > 255 or position.x < -255:
+		self.queue_free()
+	if position.y > 100 or position.y < -100:
+		self.queue_free()
+
+func move_debris():
+	if is_monster:
+		position.x += randi_range(-5, 5)
+		position.y -= randfn(10, 5)
+	else:
+		position.y += 5
+
+
+func _on_area_2d_mouse_entered() -> void:
+	mouse_hover = true
+
+
+func _on_area_2d_mouse_exited() -> void:
+	mouse_hover = true
+
+
+func _on_tick():
+	move_debris()
+	check_position()
