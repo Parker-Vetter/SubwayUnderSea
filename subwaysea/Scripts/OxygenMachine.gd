2b09519c48d368759cd8cd8a3f94d432fa045097
Aria-Williams <159070680+Area9579@users.noreply.github.com>
2024-11-18 13:13:27 -0600

    Face Display Initial Commit
    Display to show sanity delta and total sanity visually

--- subwaysea/Scripts/OxygenMachine.gd
+++ subwaysea/Scripts/OxygenMachine.gd
@@ -5,10 +5,17 @@ var multiplier = 1
 var oxygenAmount = 100
 var oxygenCap = 100
 
+@onready var face_display: Node2D = $"../FaceDisplay"
 @onready var playerOxyTank = get_parent().find_child("PlayerOxyTank").get_child(0)
+
+signal oxygen_threshold_changed(oxygen_threshold)
+
+func _ready() -> void:
+	self.connect("oxygen_threshold_changed", face_display._on_oxygen_threshold_changed)
 
 func _process(delta: float) -> void:
 	playerOxyTank.set("value", oxygenAmount)
+	determine_oxygen_threshold()
 
 func addOxygen():
 	if oxygenAmount < oxygenCap:
@@ -16,6 +23,14 @@ func addOxygen():
 
 func loseOxygen():
 	oxygenAmount -= 1 * multiplier
+
+func determine_oxygen_threshold():
+	var oxygen_threshold
+	if oxygenAmount < 50:
+		oxygen_threshold = "low"
+	else:
+		oxygen_threshold = "high"
+	oxygen_threshold_changed.emit(oxygen_threshold)
 
 func _on_fill_up_area_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
