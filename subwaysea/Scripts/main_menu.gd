01f4829c59e8b36d65b1d7d80c8bc1e82d8243b1
Phoenix Jordan <136138123+BirdOfAsh@users.noreply.github.com>
2024-11-21 23:58:07 -0600

    Revert "Merge branch 'Lighting_System'"
    This reverts commit 5d3e11a5a1f9eedb5548ad71e97ec8af635c4369, reversing
    changes made to a03342a3549a12d94c7ef692156284df609d9ef6.

--- subwaysea/Scripts/main_menu.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/test_scene.tscn")
-
-
-func _on_button_pressed() -> void:
-	get_tree().quit()
