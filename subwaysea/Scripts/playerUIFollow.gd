fb9c4f6d154ea6009b469f24af340c8c2cf30559
furiousknight25 <48182589+furiousknight25@users.noreply.github.com>
2024-11-25 02:09:06 -0600

    fixed oxygen

--- subwaysea/Scripts/playerUIFollow.gd
+++ subwaysea/Scripts/playerUIFollow.gd
@@ -1,9 +1,10 @@
 extends Control
 
-@onready var playerPosition = get_parent().find_child("Player").find_child("UpperBody").find_child("Line2D").find_child("RShoulder")
+@onready var playerPosition = get_parent().find_child("Player").find_child("UpperBody").find_child("Line2D").find_child("OxyTank").find_child('TextureProgressBar')
 
-var offset = Vector2(-30 , 10)
+var offset = Vector2(0 , 0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position = playerPosition.position + offset
+	playerPosition.value = $TextureProgressBar.value
+	
