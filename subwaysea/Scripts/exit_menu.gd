67d0773c9d1545a790a1732ffcc24b7b9dba69ae
Parker Vetter <vette107@d.umn.edu>
2024-11-25 13:15:57 -0600

    added in new scene, credits added and all buttons have correct functionality

--- subwaysea/Scripts/exit_menu.gd
+++ subwaysea/Scripts/exit_menu.gd
@@ -1,8 +1,10 @@
 extends Control
 
-
 func _on_credits_button_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/Credits.tscn")
 
 func _on_exit_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+
+func _on_return_button_pressed() -> void:
+	self.visible = false
