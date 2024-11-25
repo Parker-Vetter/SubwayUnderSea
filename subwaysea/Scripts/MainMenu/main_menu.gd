17b20e490f9c0c7edaef82c928a9f3dd1f3a20a1
Phoenix Jordan <136138123+BirdOfAsh@users.noreply.github.com>
2024-11-25 17:33:16 -0600

    fixed everything

--- subwaysea/Scripts/MainMenu/main_menu.gd
+++ subwaysea/Scripts/MainMenu/main_menu.gd
@@ -4,6 +4,8 @@ extends Control
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/MainScene.tscn")
 
+func _on_credits_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Credits.tscn")
 
 func _on_button_pressed() -> void:
 	get_tree().quit()
--- subwaysea/Scripts/director.gd
+++ subwaysea/Scripts/director.gd
@@ -7,20 +7,21 @@ var done = false
 
 func _process(delta):
 	if Input.is_action_just_pressed("exit"):
-		# Attempt to find MainScene dynamically
-		var MainScene = get_tree().root.get_node_or_null("MainScene")
-		if MainScene == null:
-			# MainScene is not currently in the scene tree; do nothing
-			return
-		else:
-			# Find escMenu as a child of MainScene
-			var escMenu = MainScene.get_node_or_null("escMenu")
-			if escMenu.visible == false:
-				escMenu.visible = true
-				Engine.time_scale = 0  # Pause the game
-			elif escMenu.visible == true:
-				escMenu.visible = false
-				Engine.time_scale = 1  # Resume the game
+		get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+		## Attempt to find MainScene dynamically
+		#var MainScene = get_tree().root.get_node_or_null("MainScene")
+		#if MainScene == null:
+			## MainScene is not currently in the scene tree; do nothing
+			#return
+		#else:
+			## Find escMenu as a child of MainScene
+			#var escMenu = MainScene.get_node_or_null("escMenu")
+			#if escMenu.visible == false:
+				#escMenu.visible = true
+				#Engine.time_scale = 0  # Pause the game
+			#elif escMenu.visible == true:
+				#escMenu.visible = false
+				#Engine.time_scale = 1  # Resume the game
 	
 	if Input.is_action_just_pressed("fullscreen"):
 		if !fullscreen:
@@ -29,7 +30,7 @@ func _process(delta):
 		else:
 			fullscreen = false
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
-	if Input.is_action_just_pressed('restart'):
-		#killing player instead of just reloading the scene for the nice 
-		#fade in/fade out that's already implemented
-		get_tree().reload_current_scene()
+	#if Input.is_action_just_pressed('restart'):
+		##killing player instead of just reloading the scene for the nice 
+		##fade in/fade out that's already implemented
+		#get_tree().reload_current_scene()
