110985fb9542a91fd14e7eb34fc81b81fee5d46b
Parker Vetter <vette107@d.umn.edu>
2024-12-01 22:52:02 -0600

    fixed main menu to call tutorial correctly

--- subwaysea/Scripts/MainMenu/main_menu.gd
+++ subwaysea/Scripts/MainMenu/main_menu.gd
@@ -11,7 +11,7 @@ func _on_button_pressed() -> void:
 	get_tree().quit()
 
 func _on_tutorial_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/tutorial_menu.tscn")	
+	get_tree().change_scene_to_file("res://Scenes/TutorialScene.tscn")	
 
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
